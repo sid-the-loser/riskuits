fbdbe78e5c9d888e5ef17e0a3bb7a613ed6c8151
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-10-15 21:38:53 -0400

    added a functionality to rail cam made it so it uses an exported enum to determine which axis to move on unfortunately, it can only move in x or y or z not a combination or average of both will figure out a fix for that soon.

--- scripts/rail_cam.gd
+++ scripts/rail_cam.gd
@@ -1,5 +1,26 @@
 class_name RailCamera3D
 extends Camera3D
 
-@export var look_at_object: Node3D
-@export var rail_axis: Vector3 = Vector3(0, 0, 1)
+@export var look_at_main_subject_: bool = true
+@export var main_subject_: Node3D
+@export var movement_speed_: float = 1
+@export_enum("x", "y", "z") var rail_movement_axis = "x"
+
+var feasible_position: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	feasible_position = get_position()
+
+func _process(delta: float) -> void:
+	if look_at_main_subject_:
+		look_at(main_subject_.get_position())
+		
+	match rail_movement_axis:
+		"x":
+			feasible_position.x = main_subject_.get_position().x
+		"y":
+			feasible_position.y = main_subject_.get_position().y
+		"z":
+			feasible_position.z = main_subject_.get_position().z
+	
+	set_position(get_position().lerp(feasible_position, movement_speed_ * delta))
