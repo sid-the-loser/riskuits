04ee86a2340a4469a9007d6625bcb8de1f058c72
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-10-18 20:32:20 -0400

    made a couple of changes harrish wanted me to make

--- scripts/billboard_sprite.gd
+++ scripts/billboard_sprite.gd
@@ -1,10 +1,14 @@
 class_name BillboardSprite
 extends Sprite3D
 
-var current_camera: Camera3D = null
-
-func _ready() -> void:
-	current_camera = get_viewport().get_camera_3d()
-	
-func _process(delta: float) -> void:
-	look_at(current_camera.get_position())
+#var current_camera: Camera3D = null
+#var converted_pos: Vector3 = Vector3.ZERO
+#
+#func _ready() -> void:
+	#current_camera = get_viewport().get_camera_3d()
+	#converted_pos.y = position.y
+	#
+#func _process(delta: float) -> void:
+	#converted_pos.x = current_camera.get_position().x
+	#converted_pos.z = current_camera.get_position().z
+	#look_at(converted_pos)
