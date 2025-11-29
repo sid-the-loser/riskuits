0760720519af8d764377e8248eb583bda6f58e80
Sidharth <sidharthsuresh07@gmail.com>
2025-11-29 03:21:05 -0500

    large chunks of changes are being pushed here

---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+const HALLWAY = preload("uid://d0jasc8dyycal")
+
+
+func _on_play_button_button_down() -> void:
+	GameManager.tutorial_flag = true
+	get_tree().change_scene_to_packed(HALLWAY)
+
+
+func _on_quit_button_button_down() -> void:
+	get_tree().quit()
