07289f915c6d2367dd5993b614e797c858af4cc3
Sidharth <sidharthsuresh07@gmail.com>
2025-12-01 00:44:04 -0500

    Add win/lose UI controls and update interaction logic in hallway scene

--- scenes/hallway.gd
+++ scenes/hallway.gd
@@ -3,12 +3,16 @@ extends Node3D
 
 var talking_to_ginna_flag = false
 var can_talk_to_ginna_flag = false
+var ended_flag = false
 
 @onready var player: CharacterBody3D = $Player
 @onready var pause_menu: Control = $PauseMenu
 
 @onready var ginna: Sprite3D = $Ginna
 @onready var interaction_icon: Sprite3D = $InteractionIcon
+
+@onready var win_control: Control = $Win
+@onready var lose_control: Control = $Lose
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,28 +28,32 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Dialogic.current_timeline == null:
+	if Dialogic.current_timeline == null and !GameManager.tutorial_flag:
 		if talking_to_ginna_flag:
 			if Dialogic.VAR.get_variable("bagged"):
-				print("Winner!")
-			else:
-				print("LOSER")
-				
-			talking_to_ginna_flag = false
-			
-		if Input.is_action_just_pressed("back"):
-			if GameManager.using_ui:
-				GameManager.using_ui = false
-				pause_menu.hide()
-				
+				GameManager.using_ui = true
+				win_control.show()
 			else:
 				GameManager.using_ui = true
-				pause_menu.show()
-			
-		if Input.is_action_just_pressed("talk") and can_talk_to_ginna_flag:
-			Dialogic.start("ginna")
-			print(Dialogic.VAR.get_variable("bagged"))
-			talking_to_ginna_flag = true
+				win_control.show()
+				
+			ended_flag = true
+			talking_to_ginna_flag = false
+		
+		if !ended_flag:
+			if Input.is_action_just_pressed("back"):
+				if GameManager.using_ui:
+					GameManager.using_ui = false
+					pause_menu.hide()
+					
+				else:
+					GameManager.using_ui = true
+					pause_menu.show()
+				
+			if Input.is_action_just_pressed("talk") and can_talk_to_ginna_flag:
+				Dialogic.start("ginna")
+				print(Dialogic.VAR.get_variable("bagged"))
+				talking_to_ginna_flag = true
 
 func _on_classroom_entrance_body_entered(body: Node3D) -> void:
 	if GameManager.tutorial_flag and body.name == "Player":
