35a8cf2ebfcbd29a6e15314b481074de6bf1348f
Sidharth <sidharthsuresh07@gmail.com>
2025-11-30 22:22:33 -0500

    Refactor interaction handling and update hallway scene structure

--- scenes/hallway.gd
+++ scenes/hallway.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 
+var talking_to_ginna = false
+var can_talk_to_ginna = false
+
 @onready var player: CharacterBody3D = $Player
 @onready var pause_menu: Control = $PauseMenu
 
@@ -10,23 +13,41 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pause_menu.hide()
+	interaction_icon.hide()
 	if GameManager.tutorial_flag:
+		ginna.hide()
 		Dialogic.start("pre_classroom_hallway")
 	else:
+		ginna.show()
 		player.position = Vector3(-13.212, 1.0, -23.651)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("back") and Dialogic.current_timeline == null:
-		if GameManager.using_ui:
-			GameManager.using_ui = false
-			pause_menu.hide()
+	if Dialogic.current_timeline == null:
+		if Input.is_action_just_pressed("back"):
+			if GameManager.using_ui:
+				GameManager.using_ui = false
+				pause_menu.hide()
+				
+			else:
+				GameManager.using_ui = true
+				pause_menu.show()
 			
-		else:
-			GameManager.using_ui = true
-			pause_menu.show()
+		if Input.is_action_just_pressed("talk") and can_talk_to_ginna:
+			talking_to_ginna = true
 
 func _on_classroom_entrance_body_entered(body: Node3D) -> void:
 	if GameManager.tutorial_flag and body.name == "Player":
 		get_tree().call_deferred("change_scene_to_file", "res://scenes/classroom.tscn")
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if not GameManager.tutorial_flag and body.name == "Player":
+		interaction_icon.show()
+		can_talk_to_ginna = true
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if not GameManager.tutorial_flag and body.name == "Player":
+		interaction_icon.hide()
+		can_talk_to_ginna = false
